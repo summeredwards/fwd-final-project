e8fb5e2ba1b6e21d4a4f1de21fa41fb744a9289c
anhugenberg <anhugenberg@gmail.com>
2022-11-27 17:30:02 -0700

    add base hangman code example

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const  Header = () => {
+    return(
+        <>
+            <h1>Hangman</h1>
+            <p>Enter letters using your keyboard to figure out the word!</p>
+        </>
+    )
+}
+
+export default Header;
