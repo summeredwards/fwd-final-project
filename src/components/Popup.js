d6a7ae27a339d8034f32b40b0e9a8bc58a662cf9
anhugenberg <anhugenberg@gmail.com>
2022-11-29 10:43:01 -0700

    changes made during 11/29 class

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,15 +3,13 @@ import { checkWin } from '../helpers/helpers';
 
 const  Popup = ({ correctLetters, wrongLetters, selectedWord, setPlayable, playAgain }) => {
     let finalMessage= '';
-    let finalMessageRevealWord = '';
     let playable = true;
 
     if(checkWin(correctLetters, wrongLetters, selectedWord) === 'win' ){
-        finalMessage = 'Congratulations! You won! 😃';
+        finalMessage = 'Congratulations! You won!';
         playable = false;
     } else if(checkWin(correctLetters, wrongLetters, selectedWord) === 'lose'){
-        finalMessage = 'Unfortunately you lost. 😕';
-        finalMessageRevealWord = `...the word was: ${selectedWord}`;
+        finalMessage = 'Game over. You lost.';
         playable = false;
     }
 
@@ -22,7 +20,6 @@ const  Popup = ({ correctLetters, wrongLetters, selectedWord, setPlayable, playA
             <div className="popup-container" style={finalMessage !== '' ? {display:'flex'} : {}}>
                 <div className="popup">
                     <h2>{finalMessage}</h2>
-                    <h3>{finalMessageRevealWord}</h3>
                     <button onClick={playAgain}>Play Again</button>
                 </div>
             </div>
