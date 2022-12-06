fd9face3699ad6bd7dcd100d6e8f4d1d26789005
anhugenberg <anhugenberg@gmail.com>
2022-12-06 10:58:22 -0700

    change title of game

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const  Header = ({categoryName}) => {
     return(
         <>
-            <h1>Hangman</h1>
+            <h1>Consequences</h1>
             <p>Enter letters using your keyboard to figure out the word!</p>
             <p>Category is: {categoryName}</p>
         </>
